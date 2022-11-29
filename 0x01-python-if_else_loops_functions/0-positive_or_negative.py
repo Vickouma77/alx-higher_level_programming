88b47fe690176a8eba435e369f182bbb80d0120e
vickouma77 <vickprestone20@gmail.com>
2022-11-29 03:46:30 -0800

    including the f-string

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(number)
+    print(f"number is positive {number}")
 elif number == 0:
-    print(number)
+    print(f"number is equal to zero {number}")
 else:
-    print(number)
+    print(f"number is negative {number}")
