9d4cec775e68c5cda11a1862920f4f62af8089e7
vickouma77 <vickprestone20@gmail.com>
2022-11-29 04:47:31 -0800

    Task1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+number_2 = abs(number) % 10
+if number_2 < 0:
+    number_2 = -number_2
+print(f"Last string of {number} is {number_2}")
+if number_2 > 5:
+    print("greater than 5")
+elif number_2 == 0:
+    print("is 0")
+else:
+    print("is less than 6 and not 0")
