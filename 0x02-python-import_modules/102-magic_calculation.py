4c5a931eaa51ca3dd07c2378934a085a5ca40111
vickouma77 <vickprestone20@gmail.com>
2022-12-03 09:40:41 -0800

    second magic commit

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,9 +3,10 @@ def magic_calculation(a, b):
     from magic_calculation import add, sub
 
     if a < b:
-        c = add(a, b):
-            for i in range (6, 8):
-                c = add(c, i)
-                return (c)
-            else:
-                return(sub(a, b))
+        c = add(a, b)
+        for i in range(6, 8):
+            c = add(c, i)
+        return(c)
+
+        else:
+            return(sub(a, b))
