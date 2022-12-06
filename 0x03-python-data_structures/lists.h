4548c02ae5b0470b0354e4253e3afdbc680505fc
vickouma77 <vickprestone20@gmail.com>
2022-12-06 02:51:15 -0800

    header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,18 +1,21 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -21,6 +24,5 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 int rec_palindrome(listint_t **head, listint_t *tail);
-
 
 #endif /* LISTS_H */
