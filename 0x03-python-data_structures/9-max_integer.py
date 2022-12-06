2e0644c082b2fa4a728b2f85d5f752f5d57c3bdc
vickouma77 <vickprestone20@gmail.com>
2022-12-06 02:21:19 -0800

    Task9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    max_n = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > max_n:
+            max_n = my_list[i]
+    return (max_n)
