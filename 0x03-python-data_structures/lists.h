d8787dde39aed60953e139a4e0579cd24445c5d8
vickouma77 <vickprestone20@gmail.com>
2022-12-06 02:45:26 -0800

    adding lists header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -20,5 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int rec_palindrome(listint_t **head, listint_t *tail);
+
 
 #endif /* LISTS_H */
