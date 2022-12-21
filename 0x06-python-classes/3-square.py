75a2b6794af2c1e38a239f31fb3ddce589fd1bb3
vickouma77 <vickprestone20@gmail.com>
2022-12-21 02:27:33 -0800

    updating square-3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,5 +14,9 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            def area(self):
-                return(square * square)
+            self.__size = size
+
+    def area(self):
+        """return the current square area
+        """
+        return(size * size)
