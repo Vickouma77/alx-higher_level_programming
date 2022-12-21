def20ddcd24060be8f783660166b96c798ebdf7a
vickouma77 <vickprestone20@gmail.com>
2022-12-21 03:10:15 -0800

    adding square-4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,7 +19,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if type(size) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
