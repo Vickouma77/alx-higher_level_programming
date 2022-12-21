6f71b31625f52e766d8a9f0d2412f5bfa5f212bf
vickouma77 <vickprestone20@gmail.com>
2022-12-20 22:18:15 -0800

    adding one square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """class Square that defines a square
+    """
+    def __init__(self, size):
+        """Initialize method that stores the size of the square
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
