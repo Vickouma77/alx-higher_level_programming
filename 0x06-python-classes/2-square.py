7b95da898526235742f6c384aace0aada659276e
vickouma77 <vickprestone20@gmail.com>
2022-12-21 01:18:55 -0800

    adding square-2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""creating class square
+"""
+
+
+class Square:
+    """class Square that defines a square """
+
+    def __init__(self, size=0):
+        """instantiating of Private instance attribute
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError(size must be >= 0)
+        else:
+            self.__size = size
