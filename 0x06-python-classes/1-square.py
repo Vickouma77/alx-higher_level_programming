c5f00588c50364c2d1075045f370d64195f00995
vickouma77 <vickprestone20@gmail.com>
2022-12-20 23:43:09 -0800

    adding main directory

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,7 +4,9 @@ class Square:
     """
     def __init__(self, size):
         """Initialize method that stores the size of the square
+
         Args:
             param1 (int): size of the square
         """
+
         self.__size = size
