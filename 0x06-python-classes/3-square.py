4026d6ec5ff45c71ee284e3ceefa8aa081215981
vickouma77 <vickprestone20@gmail.com>
2022-12-21 02:16:03 -0800

    adding square-3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""creating class Square
+"""
+
+
+class Square:
+    """class square that defines a square
+    """
+    def __init__(self, size=0):
+        """instantiating private instance attribute
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            def area(self):
+                return(square * square)
