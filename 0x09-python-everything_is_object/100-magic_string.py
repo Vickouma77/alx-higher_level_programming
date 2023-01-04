c84996c994b1dee341a17110362530972f5658a5
vickouma77 <vickprestone20@gmail.com>
2023-01-04 14:02:38 -0800

    updating magic_string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.ntimes = getattr(magic_string, 'ntimes', 0) + 1
-    return ("Holberton, " * (magic_string.ntimes - 1) + "BestSchool")
+def magic_string(h=[]):
+    h += ["BestSchool"]
+    return ", ".join(h)
