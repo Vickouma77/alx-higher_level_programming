34cf152df1ba82c5fcc1e2653492017abb9e68b0
vickouma77 <vickprestone20@gmail.com>
2023-01-09 14:47:05 -0800

    adiing base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Implements the BaseGeometry class"""
+
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
