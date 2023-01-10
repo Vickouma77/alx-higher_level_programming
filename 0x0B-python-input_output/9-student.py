93619eec73845a195fd9ea011fbe76c617df7456
vickouma77 <vickprestone20@gmail.com>
2023-01-10 14:39:00 -0800

    creating Student class

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,11 +9,10 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Defines instation of class Student"""
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
         def to_json(self):
-            """Retrieves dictionary representation of Student class"""
+            """Method returns directory description"""
             return self.__dict__.copy()
