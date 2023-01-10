c4e8a7309ed37d8985787822970fb539c316684f
vickouma77 <vickprestone20@gmail.com>
2023-01-10 10:24:17 -0800

    adding read file wit encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -15,5 +15,6 @@ def read_file(filename=""):
 
     """
 
-    with open("filename", encoding="UTF-8") as f:
-        print(f.read(), end='')
+    with open("filename", encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
