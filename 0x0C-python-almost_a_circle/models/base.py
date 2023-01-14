b8bf3975a6310534fb2d76b0c27c641af1200bd0
vickouma77 <vickprestone20@gmail.com>
2023-01-14 13:35:18 -0800

    creating base.py to mange 'id'

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,4 +2,17 @@
 
 """Writes the first class base"""
 
+class Base:
+    """A base class
+    """
+    __nb_objects = 0
 
+    def __init__(self, id=None):
+        """instansciating id with value of none
+        """
+
+        if id is not none:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = id
