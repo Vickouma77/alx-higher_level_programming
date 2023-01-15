589165cf9a8fe3b005a0b1d0b5a4540be82e0964
vickouma77 <vickprestone20@gmail.com>
2023-01-15 04:24:02 -0800

    inheriting base

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module with class Rectangle which inherits from Base
+"""
+
+from models.Base import  Base
+
+
+class Rectangle(Base):
+    """class Rectangle which inherits Base properties"""
+
+    __width -> width
+    __height -> height
+    __x -> x
+    __y -> y
+    super().__init__(id)
