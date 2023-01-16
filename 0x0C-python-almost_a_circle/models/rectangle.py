8f61416876ec3faf2c35e0b53cd6a6ca3d77f784
vickouma77 <vickprestone20@gmail.com>
2023-01-16 11:35:31 -0800

    updating rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 """Module with class Rectangle which inherits from Base
 """
-
 from models.base import  Base
 
 
 class Rectangle(Base):
-    """class Rectangle which inherits Base properties"""
+    """class Rectangle which inherits Base properties
+
+     Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+    """
 
     self.width = width
     self.height = height
