af12fd9315f88c1d26d8305831db7b948834935c
vickouma77 <vickprestone20@gmail.com>
2023-01-16 13:22:01 -0800

    updating triangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -137,4 +137,20 @@ class Rectangle(Base):
                 elif k == "x":
                     self.x = v
                 elif k == "y":
-                    self.y = v         
+                    self.y = v
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)      
