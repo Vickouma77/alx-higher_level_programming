95597fe544141c09c99dc33cfca14576c7e32557
vickouma77 <vickprestone20@gmail.com>
2023-02-14 11:52:29 -0800

    listing tables

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- table called first_table in the current database in your MySQL server
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT
-	name VARCHAR(256)
-	);
+CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
