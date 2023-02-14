1ac7c75b78273c474dd2fc8216a42870c316107b
vickouma77 <vickprestone20@gmail.com>
2023-02-14 12:40:24 -0800

    adding change class

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,3 @@
--- Updates the score of Bob to 10 in the table second_table.
-UPDATE `second_table`
-SET `score` = 10
-WHERE `name` = "Bob";
+-- Removes all records with a score <= 5 in the table second_table.
+DELETE FROM `second_table`
+WHERE `score` <= 5;
