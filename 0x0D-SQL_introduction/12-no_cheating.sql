459241b42f398fc5560c53fd6be27c3d1644dad6
vickouma77 <vickprestone20@gmail.com>
2023-02-14 12:38:11 -0800

    adding no cheating

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,6 +1,4 @@
--- Lists all records in the table second_table with a score >= 10.
--- Records are ordered by descending score.
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `score` >= 10
-ORDER BY `score` DESC;
+-- Updates the score of Bob to 10 in the table second_table.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
