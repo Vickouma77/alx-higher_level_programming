a4c5117e6e863a7b2b5847c07a753615245b5436
vickouma77 <vickprestone20@gmail.com>
2023-02-14 11:55:39 -0800

    listing tables

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- table called first_table in the current database
-CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
