8eb958d9e0e7e88b0c7d4c2daac1be8c41b67542
vickouma77 <vickprestone20@gmail.com>
2023-02-15 08:44:06 -0800

    creating read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-ON 'user_0d_2'@'localhost'
-FOR hbtn_0d_2
+ON 'hbtn_0d_2'.*
+TO 'user_0d_2'@'localhost'
