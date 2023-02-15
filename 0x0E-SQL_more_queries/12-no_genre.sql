fb1bc42e0122458101c6777980e60a4489afc5fa
vickouma77 <vickprestone20@gmail.com>
2023-02-15 12:13:56 -0800

    no genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- list all shows in database
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ ORDER BY s.`title`, g.`genre_id`;
