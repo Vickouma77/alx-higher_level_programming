c8814cbf74b3fc90950fc947867e4bc3b3c33ef3
vickouma77 <vickprestone20@gmail.com>
2023-02-15 11:52:43 -0800

    adding cities of california

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,1 +1,8 @@
--- 
+--  lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	        FROM `states`
+	       WHERE `name` = "California")
+ORDER BY `id`;
