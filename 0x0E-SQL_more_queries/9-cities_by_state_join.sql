f257a6adfe281508a0cb0ff68b51c81b9cf0ebea
vickouma77 <vickprestone20@gmail.com>
2023-02-15 11:57:24 -0800

    adding cities state

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- script that lists all cities contained in the database
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
