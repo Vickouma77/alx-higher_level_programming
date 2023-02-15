859eac994ef04de8a8b8143a2840609226d0d6b2
vickouma77 <vickprestone20@gmail.com>
2023-02-15 05:52:46 -0800

    listing privilages

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- listing all privilages for user
-SHOW GRANT FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANT FOR 'user_0d_1'@'localhost'; 
+SHOW GRANT FOR 'user_0d_2'@'localhost';
