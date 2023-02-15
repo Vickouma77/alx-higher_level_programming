1fd9414a34c6821dd3a8c0379c886539ef167537
vickouma77 <vickprestone20@gmail.com>
2023-02-15 08:02:38 -0800

    craeting new user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,1 +1,7 @@
--- creating new user with all privilages
+-- creating new user with all privileges
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
