ba87092c02860713d036e911092655e0726f40e0
vickouma77 <vickprestone20@gmail.com>
2023-02-15 12:10:12 -0800

    genre id

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,1 +1,6 @@
---
+-- database dump of hbtn_0d_tvshows
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
