da876ac526f8ed6c6574cee62007307c5834d790
vickouma77 <vickprestone20@gmail.com>
2023-02-15 12:28:12 -0800

    shows by genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,1 +1,9 @@
 -- dump from hbtn_0d_tvshows to your MySQL server
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON s.`genre_id` = g.`id`
+ ORDER BY t.`title`, g.`name`;
