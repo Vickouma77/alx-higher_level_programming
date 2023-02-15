6b92925baf17cedbec4d140682b93eb52aa10f93
vickouma77 <vickprestone20@gmail.com>
2023-02-15 12:25:19 -0800

    comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,1 +1,10 @@
 -- database dump from hbtn_0d_tvshows to your MySQL server
+SELECT t.`title`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
