805ed01fc9b79fad68efe6b7132b55d71e35fcea
vickouma77 <vickprestone20@gmail.com>
2023-02-15 11:38:04 -0800

    adding state table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,5 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`state`(PRIMARY KEY(`id`), id`   INT          NOT NULL AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`state`(
+	PRIMARY KEY(`id`), id`   INT        NOT NULL AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL
+);
