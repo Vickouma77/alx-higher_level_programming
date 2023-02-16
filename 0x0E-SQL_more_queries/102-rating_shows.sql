abf8d4125e0a43dee64176ffbffd278e7a68c893
vickouma77 <vickprestone20@gmail.com>
2023-02-16 04:58:59 -0800

    rating shows

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,1 +1,7 @@
 -- listing all genres
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
