3f586c75e723c1c889d03cb5db859060485eba35
vickouma77 <vickprestone20@gmail.com>
2023-02-16 04:52:11 -0800

    not my genres

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,18 @@
+-- lists all genres
+SELECT DISTINCT `name`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS t
+       ON s.`show_id` = t.`id`
+       WHERE g.`name` NOT IN
+             (SELECT `name`
+                FROM `tv_genres` AS g
+	             INNER JOIN `tv_show_genres` AS s
+		     ON g.`id` = s.`genre_id`
+
+		     INNER JOIN `tv_shows` AS t
+		     ON s.`show_id` = t.`id`
+		     WHERE t.`title` = "Dexter")
+ ORDER BY g.`name`;
