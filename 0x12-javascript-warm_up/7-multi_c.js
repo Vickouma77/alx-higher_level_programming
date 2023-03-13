e73e3402c18ac496a05d9ae12b59f79232bcdc0e
vickouma77 <Vickprestone20@gmail.com>
2023-03-14 00:17:54 +0300

    multi c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	const x = parseInt(process.argv[2]);
-	if (isNaN(x)) {
-		console.log('Missing number of occurrences');
-	} else {
-		const i = 0;
-		while (i < x) {
-			console.log('C is fun');
-			i++;
-		}
-	}
+  const x = parseInt(process.argv[2]);
+  if (isNaN(x)) {
+    console.log('Missing number of occurrences');
+  } else {
+    for (let i = 0; i < x; i++) {
+      console.log('C is fun');
+    }
+  }
 }
