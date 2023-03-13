b5192fef03370fceb7eb4f1e00a1f9cf51a3198a
Vickouma77 <Vickprestone20@gmail.com>
2023-03-13 04:21:09 -0700

    3 languages script

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 
-const myVar = ('C is fun\nPython is cool\nJavaSciript is amazing');
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
