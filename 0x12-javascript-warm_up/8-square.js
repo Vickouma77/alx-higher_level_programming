9c063a60b0ff1159cabd994bcf8ad8162d7c981f
vickouma77 <Vickprestone20@gmail.com>
2023-03-14 15:23:56 +0300

    printing squares

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (isNaN(size)) {
 } else {
   for (let i = 0; i < size; i++) {
     let square = '';
-    for (let j = 0; j < size; j++) square += 'x';
+    for (let j = 0; j < size; j++) square += 'X';
     console.log(square);
   }
 }
