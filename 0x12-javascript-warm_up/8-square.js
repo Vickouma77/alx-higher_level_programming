a59a58462aaca4e2aab3fc6b5b0e79e28098627c
vickouma77 <Vickprestone20@gmail.com>
2023-03-14 15:18:36 +0300

    printing squares

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
-
+const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  let square = '';
   for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      square += 'x';
-    }
-    square += '\n';
+    let square = '';
+    for (let j = 0; j < size; j++) square += 'x';
+    console.log(square);
   }
-  console.log(square);
 }
