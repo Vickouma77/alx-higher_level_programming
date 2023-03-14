6d9b40e1f75818fd602aff684c5ae14f6ce19e13
vickouma77 <Vickprestone20@gmail.com>
2023-03-14 19:15:21 +0300

    rectangle2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 && h <= 0) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       return {};
     } else {
       this.width = w;
