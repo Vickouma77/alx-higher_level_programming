a402fc67a0a95eb262f3b755e4e512dbe7b1d0b5
vickouma77 <Vickprestone20@gmail.com>
2023-03-17 23:35:48 +0300

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(
-            host="localhos",
+            host="localhost",
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.arv[3],
