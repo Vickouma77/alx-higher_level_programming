ebf22330c704a6f5afe1b546fc8656f377bd8318
vickouma77 <Vickprestone20@gmail.com>
2023-03-17 23:42:23 +0300

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
             host="localhost",
             user=sys.argv[1],
             passwd=sys.argv[2],
-            db=sys.arv[3],
+            db=sys.argv[3],
             port=3306
     )
     cursor = db.cursor()
