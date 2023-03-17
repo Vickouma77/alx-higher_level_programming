ce753e87498ac7fe0f584643389c19a344ad1f6a
vickouma77 <Vickprestone20@gmail.com>
2023-03-18 00:02:07 +0300

    my filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states
+   table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            states=input(" "),
+            port=3306)
+    cursor = db.cursor()
+    cursor.execute(""""SELECT * FROM states
+                   WHERE name = '{}'".format(states)
+                   """)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
