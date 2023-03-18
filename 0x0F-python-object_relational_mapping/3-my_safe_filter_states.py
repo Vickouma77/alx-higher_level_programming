ddede9f0fe400443e6d4f5b2c33e1ff41cb9b1d3
vickouma77 <Vickprestone20@gmail.com>
2023-03-18 10:26:56 +0300

    my safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""write a script that takes in arguments and displays
+   all values in the states table of hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306)
+    cursor = db.cursor
+    states_name = sys.argv[4]
+    cursor.execute("SELECT * FROM state WHERE name LIKE '%s'" (states_name, ))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
