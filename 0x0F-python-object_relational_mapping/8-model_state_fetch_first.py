77de8d625d75b90a3f860187bd3be539adb77d64
vickouma77 <Vickprestone20@gmail.com>
2023-03-18 19:43:36 +0300

    8-model state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,7 @@ if __name__ = "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).order_by(State.id).first()
+    state = session.query(State).first()
     if state is None:
         print("Nothing")
     else:
