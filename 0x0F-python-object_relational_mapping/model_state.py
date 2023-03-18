dac5fb47be0644f7403a8e5efd3d971a65b58c6d
vickouma77 <Vickprestone20@gmail.com>
2023-03-18 13:04:28 +0300

    model states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""python file that contains the class definition of a State
-   python file that contains the class definition of a State
+"""Start link class to table in database
 """
-
 import sys
-from model_state import Base, States
-from sqlalchemy import create_engine
+from model_state import Base, State
 
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
