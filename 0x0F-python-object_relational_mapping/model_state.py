352e4a47865fd83bfcb80f49b2d25850b9f36820
vickouma77 <Vickprestone20@gmail.com>
2023-03-18 12:49:31 +0300

    model states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,7 +8,8 @@ from model_base import Base, States
 
 from sqlalchemy import create_engine
 
-
 if __name__ == "__main__":
-     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                             pool_pre_ping=True)
      Base.metadata.create_all(engine)
