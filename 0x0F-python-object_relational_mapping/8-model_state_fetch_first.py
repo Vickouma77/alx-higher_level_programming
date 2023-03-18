68255480fd35c58e8cd4ce9a8e90415d9bbc14bd
vickouma77 <Vickprestone20@gmail.com>
2023-03-18 20:12:10 +0300

    8-model state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""script that prints the first State object from the database hbtn_0e_6_usa"""
-
+"""script that prints the first State object from the database hbtn_0e_6_usa
+"""
 
 import sys
 from model_state import Base, State
