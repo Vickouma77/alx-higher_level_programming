ba35bcc7d3ef411407fca855406a7dab4c3c02da
vickouma77 <Vickprestone20@gmail.com>
2023-03-30 16:40:16 +0300

    display size of body response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-
-if [ -z "$1" ]
-then
-    echo "Please provide a URL as argument"
-    exit 1
-fi
-
-size=$(curl -s -o /dev/null -w '%{size_download}' "$1")
-
-echo "Response body size: $size bytes"
+curl -s "$1" | wc -c
