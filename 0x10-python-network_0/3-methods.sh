4f504d5cc2a2de8ce5ffe378eddb5fc31e1302e2
vickouma77 <Vickprestone20@gmail.com>
2023-03-30 18:09:33 +0300

    displaying HTTP methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX "$1"
+curl -Is "$1" | grep "Allow:" | cut -d ":" -f 2 | cut -c 2- | rev | cut -c 2- | rev
