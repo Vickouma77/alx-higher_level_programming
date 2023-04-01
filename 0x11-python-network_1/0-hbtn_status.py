68c909cfb2346ebb9e2e877a6c3213793c0ae53e
vickouma77 <Vickprestone20@gmail.com>
2023-04-01 14:21:29 +0300

    fetching url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that fetches
+https://alx-intranet.hbtn.io/status
+"""
+
+import urllib
+
+r = requests.get('https://alx-intranet.hbtn.io/status')
+r.status_code
+
+if __name__ == __main__:
+    main
