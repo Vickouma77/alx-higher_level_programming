0f7447d3bf593623c414eda3664b119e11c5cd4f
vickouma77 <Vickprestone20@gmail.com>
2023-04-01 19:50:36 +0300

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,10 +8,9 @@ to the URL and displays the body of the response
 if __name__ == '__main__':
     import sys
     from urllib import request, error
-    url = sys.argv[1]
 
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(sys.argv[1]) as response:
             data = response.read().decode('utf-8')
     except urllib.error.HTTPError as err:
         print("Error code:", err.code)
