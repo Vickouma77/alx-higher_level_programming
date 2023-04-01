97fb052c00cafd398908da51c8679c22d822494a
vickouma77 <Vickprestone20@gmail.com>
2023-04-01 14:39:29 +0300

    fetching url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,13 @@ https://alx-intranet.hbtn.io/status
 """
 
 
-if __name__ == __main__:
+if __name__ == '__main__':
     import urllib
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as hbtn:
-        content = hbtn.read()
+        body = hbtn.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
