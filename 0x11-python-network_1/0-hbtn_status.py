2f0502adc7470b7b22843d97a287f4533c074852
vickouma77 <Vickprestone20@gmail.com>
2023-04-01 14:27:45 +0300

    fetching url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,10 @@
 https://alx-intranet.hbtn.io/status
 """
 
-import urllib
-
-r = requests.get('https://alx-intranet.hbtn.io/status')
-r.status_code
 
 if __name__ == __main__:
-    main
+    
+    import urllib
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as hbtn:
+        content = hbtn.read()
