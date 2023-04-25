be5f993a20c5aca7202d77c1167061de1ee665db
vickouma77 <Vickprestone20@gmail.com>
2023-04-25 16:24:32 +0300

    displaying status code for GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,10 +3,10 @@
 const request = require('request');
 const URL = process.argv[2];
 
-request(URL, (error, response) => {
-  if (error) {
-    console.log(error);
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
   } else {
-    console.log('code:' + response.statusCode);
+    console.log('code: ' + response.statusCode);
   }
 });
