c92a856a1975929b8193b362900ed7537c10e7f8
vickouma77 <Vickprestone20@gmail.com>
2023-04-25 13:30:55 +0300

    fixing indentations

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const file = require("file");
-const path = process.argv[2]
+const file = require('file');
+const path = process.argv[2];
 
 file.readFile(path, 'utf-8', (error, content) => {
-	if (error) {
-		console.error(error)
-	} else {
-		console.log(content)
-	}
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(content);
+  }
 });
