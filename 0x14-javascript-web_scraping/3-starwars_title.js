52001aef21717a60690fcde038a19fe801e49275
vickouma77 <Vickprestone20@gmail.com>
2023-04-25 16:40:02 +0300

    star wars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/${movieId}';
 
 request(apiUrl, (error, response, body) => {
   if (error) {
