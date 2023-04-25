57421e2da5a6be65bd3944e4c9d0bd8489892a47
vickouma77 <Vickprestone20@gmail.com>
2023-04-25 13:54:19 +0300

    writing into file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs');
 const path = process.argv[2];
+const string = process.argv[3];
 
-fs.writeFile(path, 'utf-8', (error, content) => {
+fs.writeFile(path, string, 'utf-8', (error) => {
   if (error) {
     console.error(error);
-  } else {
-    console.log(content);
   }
 });
