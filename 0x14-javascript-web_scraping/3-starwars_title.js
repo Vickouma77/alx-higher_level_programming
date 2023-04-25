242b582d8cfd4cc1566c938571d82f0708b29b6b
vickouma77 <Vickprestone20@gmail.com>
2023-04-25 16:47:07 +0300

    star wars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,12 +7,8 @@ const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
 request(apiUrl + movieId,  (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
-    const movieData = JSON.parse(body);
-    if (movieData.episode_id === parseInt(movieId)) {
-      console.log(movieData.title);
-    } else {
-      console.log(`No movie found with episode id ${movieId}`);
-    }
+  } else if (response.statusCode === 200) {
+	  const responseJs = JSON.parse(body);
+	  console.log('code: ' + response.statusCode);
   }
 });
