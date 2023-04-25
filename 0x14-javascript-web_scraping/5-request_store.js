4861be16889dc4ba5d970b3e7ad194c830987293
vickouma77 <Vickprestone20@gmail.com>
2023-04-25 23:00:43 +0300

    request_store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
+const request = require('request');
 const fs = require('fs');
-const request = require('require');
-
 
 request(process.argv[2], function (err, response, body) {
   if (err == null) {
