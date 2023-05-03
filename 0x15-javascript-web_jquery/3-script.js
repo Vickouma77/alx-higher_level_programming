0032c433bdab5462770d8738d63f680378131cc4
vickouma77 <Vickprestone20@gmail.com>
2023-05-03 23:46:32 +0300

    clicks on the tag DIV#red_header~

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $('div#red_header').click(function() {
-    $('header').addClass('red');
+$(function() {
+  $('DIV#red_header').click(function() {
+    $('HEADER').addClass('red');
   });
 });
